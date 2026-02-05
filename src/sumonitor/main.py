d916c21ed3b8139cdb2df89157fa59ac27f03b5e
SrivathsanSivakumar <sri.skumar05@gmail.com>
2026-02-05 12:30:15 -0500

    persist args entered in cli so users dont have to repeat commands

--- src/sumonitor/main.py
+++ src/sumonitor/main.py
@@ -2,10 +2,11 @@
 
 ### Entry point. init pexpect and transfer control to claude
 
-import shutil, pexpect, signal, argparse
+import shutil, pexpect, signal, argparse, sys
 from .data.log_reader import LogReader
 from .session.session_data import SessionData
 from .terminal.terminal_handler import TerminalHandler
+from .config import Config
 
 def get_args_parser():
     parser = argparse.ArgumentParser(
@@ -21,10 +22,27 @@ def get_args_parser():
 def main():
     parser = get_args_parser()
     args = parser.parse_args()
+
+    config = Config()
+    cfg = config.load_config()
+
+    # user selected some other plan than default
+    if '--plan' in sys.argv:
+        cfg['plan'] = args.plan
     
-    p = pexpect.spawn(args.path, encoding='utf-8')
+    if '--path' in sys.argv:
+        cfg['path'] = args.path
+    
+    # avoid unneccessary writes 
+    if '--plan' in sys.argv or '--path' in sys.argv:
+        config.save_config(cfg)
+
+    plan = cfg.get('plan', args.plan)
+    path = cfg.get('path', args.path)
+    
+    p = pexpect.spawn(path, encoding='utf-8')
     log_reader = LogReader()
-    th = TerminalHandler(log_reader=log_reader, pexpect_obj=p, plan=args.plan)
+    th = TerminalHandler(log_reader=log_reader, pexpect_obj=p, plan=plan)
 
     p.setwinsize(*th.get_terminal_size()) # set terminal size on launch
     signal.signal(signal.SIGWINCH, th.on_resize)
